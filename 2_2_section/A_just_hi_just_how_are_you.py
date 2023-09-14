5cffa3f67dde45e0cadd12140ec3ec2ddc6ea7c4
Nhsdkk <38581029+Nhsdkk@users.noreply.github.com>
2023-09-14 22:52:52 +0300

    fix: ex.2 added

--- 2_2_section/A_just_hi_just_how_are_you.py
+++ 2_2_section/A_just_hi_just_how_are_you.py
@@ -1,9 +1,9 @@
 def solution():
-    name = input("Как Вас зовут?")
+    name = input("Как Вас зовут?\n")
 
     print(f"Здравствуйте, {name}!")
 
-    mood = input("Как дела?")
+    mood = input("Как дела?\n")
 
     if mood == "хорошо":
         print("Я за вас рада!")
