fdc629ea4fac7ddd9efd912d952a1b0efd4fdbac
Nhsdkk <38581029+Nhsdkk@users.noreply.github.com>
2023-09-16 22:42:13 +0300

    fix: ex16 added

---
+++ 2_2_section/P_legends_of_bicycle_who_is_faster.py
@@ -0,0 +1,18 @@
+def solution():
+    speed = {
+        int(input()): "Петя",
+        int(input()): "Вася",
+        int(input()): "Толя"
+    }
+
+    keys = list(speed.keys())
+    keys.sort(reverse=True)
+
+    print(speed[keys[0]].center(8).center(24))
+    print(speed[keys[1]].center(8).ljust(24))
+    print(speed[keys[2]].center(8).rjust(24))
+    print("II".center(8)+"I".center(8)+"III".center(8))
+
+
+if __name__ == "__main__":
+    solution()
